e3e032c675acb1a5678564474e8bd569afcc34ba
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-21 02:24:48 +0300

    .env file and article detail added

---
+++ components/articles/ArticleDetail.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import classes from "./ArticleDetail.module.css";
+
+const ArticleDetail = (props) => {
+  return (
+    <section className={classes.detail}>
+      <img src={props.image} alt={props.title} />
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+    </section>
+  );
+};
+
+export default ArticleDetail;
