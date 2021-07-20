44e2b1016903cc1101895be88f2a26c4676db7ec
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-20 22:34:24 +0300

    layout added, homepage updated

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,27 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import ArticleList from "../components/articles/ArticleList";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const DUMMY_ARTICLES = [
+  {
+    id: "1",
+    title: "How to deploy your MERN app to heroku",
+    image:
+      "https://resources.mynewsdesk.com/image/upload/ar_16:9,c_fill,dpr_auto,f_auto,g_auto,q_auto,w_864/rasptjnaprtkzys4tbbu.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quidem consequuntur accusantium quos minus, debitis ex. Natus enim illum, ut molestias deserunt fuga culpa rerum necessitatibus unde? Mollitia autem beatae eius voluptatum nulla officiis sint laudantium nihil voluptas alias ex sapiente, suscipit voluptatibus inventore iure fuga doloremque ipsa totam.",
+  },
+  {
+    id: "2",
+    title: "How to deploy your React app to heroku",
+    image:
+      "https://resources.mynewsdesk.com/image/upload/ar_16:9,c_fill,dpr_auto,f_auto,g_auto,q_auto,w_864/rasptjnaprtkzys4tbbu.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quis quidem consequuntur accusantium quos minus, debitis ex. Natus enim illum, ut molestias deserunt fuga culpa rerum necessitatibus unde? Mollitia autem beatae eius voluptatum nulla officiis sint laudantium nihil voluptas alias ex sapiente, suscipit voluptatibus inventore iure fuga doloremque ipsa totam.",
+  },
+];
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const HomePage = () => {
+  return <ArticleList articles={DUMMY_ARTICLES} />;
+};
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default HomePage;
