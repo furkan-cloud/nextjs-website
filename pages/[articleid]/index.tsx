2fd48c6373ef36c54041c9d9c93ccd05836127ba
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-21 02:26:44 +0300

    article detail added

---
+++ pages/[articleid]/index.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import ArticleDetail from "../../components/articles/ArticleDetail";
+
+const index = () => {
+  return (
+    <ArticleDetail
+      title="Article one"
+      description="lorem ipsum"
+      image="https://resources.mynewsdesk.com/image/upload/ar_16:9,c_fill,dpr_auto,f_auto,g_auto,q_auto,w_864/rasptjnaprtkzys4tbbu.jpg"
+    />
+  );
+};
+
+export default index;
