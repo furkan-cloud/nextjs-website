8b82ea25751edd3d5b3147070301a82078f396ab
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-20 22:35:23 +0300

    card and main navigation bar added

---
+++ components/layout/MainNavigation.tsx
@@ -0,0 +1,22 @@
+import classes from "./MainNavigation.module.css";
+import Link from "next/link";
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Articles</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">All Articles</Link>
+          </li>
+          <li>
+            <Link href="/new-article">Add New Article</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
